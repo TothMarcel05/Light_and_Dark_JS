e26162a40100682b04c39dd8bf22042cd938e7e2
Tóth Marcel Dániel <toth.marcel.daniel@diak.szbi-pg.hu>
2025-11-10 10:58:18 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function switchTheme() {
   document.body.classList.add('bg-dark')
+  const navbar = document.getElementById('navbar')
+  navbar.classList.remove('navbar-light', 'bg-light')
+  navbar.classList.add('navbar-dark', 'bg-dark')
+ 
+ const card = document.getElementsByClassName('card')
+  card.classList.add('text-white', 'bg-dark', 'border-light ')
 }
