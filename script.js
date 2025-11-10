6933fc0dd6a55b9e0963dde40df499d382edcaa7
Tóth Marcel Dániel <toth.marcel.daniel@diak.szbi-pg.hu>
2025-11-10 11:50:44 +0100

    .

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function switchTheme() {
 
   const navbar = document.getElementById('navbar')
   const cards = document.querySelectorAll('.card')
+  const tables =  document.querySelectorAll('.table')
+  const cardColums = document.querySelectorAll(".column")
+  const footerText = document.querySelectorAll(".navbar-text")
 
   navbar.classList.remove('navbar-light', 'bg-light')
   navbar.classList.add('navbar-dark', 'bg-dark')
@@ -11,4 +14,17 @@ function switchTheme() {
   cards.forEach(card => {
     card.classList.add("text-white", "bg-dark", "border-light")
   })
+
+  tables.forEach(table => {
+    table.classList.add("table-dark")
+  })
+
+  cardColums.forEach(card => {
+    card.classList.remove("col");
+    card.classList.add("col-12");
+  })
+
+    footerText.style.paddingLeft = '1200px';
+    footerText.style.display = 'block';
+    footerText.classList.add('text-white');
 }
